be7ac3f908c907150e8bd4525b82bedb9e55976c
Lola Crispin <swetlanda@gmail.com>
2024-08-15 15:20:19 +0000

    Setting up templates log in, log out and register (to be styled later), class-base Homepage

--- travel_tasks/views.py
+++ travel_tasks/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 from django.views.generic import TemplateView
 
 
-class HomePage(TemplateView): 
+class HomePage(TemplateView):
     """
     Displays home page"
     """
-    template_name = 'index.html'
+    template_name = 'travel_tasks/home.html'
+
+
