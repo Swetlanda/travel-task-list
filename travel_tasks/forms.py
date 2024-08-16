24b1d91343cf3cf9287389757063931a68642d38
Lola Crispin <swetlanda@gmail.com>
2024-08-16 12:12:15 +0000

    Added transportation filed in forms to be visible in create new task, updated layout of Add task

--- travel_tasks/forms.py
+++ travel_tasks/forms.py
@@ -4,4 +4,4 @@ from .models import TravelTask
 class TravelTaskForm(forms.ModelForm):
     class Meta:
         model = TravelTask
-        fields = ['destination', 'description', 'start_date', 'end_date', 'book_hotel', 'status']
+        fields = ['destination', 'description', 'start_date', 'end_date', 'book_hotel', 'transportation', 'status']
